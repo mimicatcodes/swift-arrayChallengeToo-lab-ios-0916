7618f3b65587e256d896aaf86d122b410ea88261
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-09-01 14:57:28 -0400

    DONE

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -20,7 +20,41 @@ class ViewController: UIViewController {
     
     
     // Create your methods here
+    
+    func addNameToDeliLine(name:String)->String{
+        switch name {
+        case "Billy Crystal":
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Billy! You can sit wherever you like."
+        case "Meg Ryan":
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Meg! You can sit wherever you like."
+        default:
+            if deliLine.count == 0 {
+                deliLine.append(name)
+                return "Welcome \(name), you're first in line!"
+            } else {
+            deliLine.append(name)
+            return "Welcome \(name), you're number \(deliLine.count) in line."}
+        }
+                
+    }
 
+    
+    func nowServing()->String{
+        if deliLine.isEmpty {
+            return "The line is empty."
+        } else {
+            var message: String = "The line is:"
+            for(index, name) in deliLine.enumerate() {
+                message = message + " \(index+1). \(name)"
+            }
+            return message
+        }
+        
+    }
 
+    
 }
+
 
